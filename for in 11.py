7d504226d8d8c38109cc545f9f250b3ea7c47075
iqbalal13 <123553063+iqbalal13@users.noreply.github.com>
2023-02-25 19:40:38 +0700

    Add files via upload

---
+++ for in 11.py
@@ -0,0 +1,10 @@
+n = int(input("Enter a number: "))
+
+if n > 0:
+    for x in range(n-1, 0, -1):
+        print(x)
+elif n < 0:
+    for x in range(n+1, 0, 1):
+        print(x)
+else:
+    print("Invalid input")
