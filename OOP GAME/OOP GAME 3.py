cd62256c8369718a771115b21355588dcf82ee23
iqbalal13 <123553063+iqbalal13@users.noreply.github.com>
2023-03-24 23:11:44 +0700

    Update OOP GAME 3.py

--- OOP GAME/OOP GAME 3.py
+++ OOP GAME/OOP GAME 3.py
@@ -23,7 +23,7 @@ class NumberGame:
 
 def main():
     game = NumberGame()
-    print("Guess the number between 1 and 100. You have {} guesses.".format(game.max_guesses))
+    print("Guess the number between 1 and 20. You have {} guesses.".format(game.max_guesses))
 
     while True:
         guess = input("Enter your guess: ")
