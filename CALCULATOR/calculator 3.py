fd03e2e0929e47a573751892e6393836a5781654
iqbalal13 <muhammadiqbalalmaududii@gmail.com>
2023-10-18 18:21:54 +0700

    Add files via upload

---
+++ CALCULATOR/calculator 3.py
@@ -0,0 +1,31 @@
+operasi_hitung = input("Masukkan operasi hitung: ")
+angka1 = input("masukkan angka pertama: ")
+angka2 = input("masukkan angka kedua: ")
+
+try:
+    angka1 = float(angka1)
+except ValueError:
+    print("Input angka pertama salah")
+    exit()
+
+try:
+    angka2 = float(angka2)
+except ValueError:
+    print("Input angka kedua salah")
+    exit()
+
+tambah = angka1 + angka2
+kurang = angka1 - angka2
+bagi = angka1 / angka2
+kali = angka1 * angka2
+
+if operasi_hitung == "+":
+    print(tambah)
+elif operasi_hitung == "-":
+    print(kurang)
+elif operasi_hitung == "/":
+    print(bagi)
+elif operasi_hitung == "*":
+    print(kali)
+else:
+    print("Input operasi salah")
