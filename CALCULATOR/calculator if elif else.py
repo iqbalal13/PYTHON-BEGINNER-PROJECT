0a11cee204078a55f5a7afe674900df1c7c8e5d2
iqbalal13 <muhammadiqbalalmaududii@gmail.com>
2023-10-29 21:26:21 +0700

    Add files via upload

---
+++ CALCULATOR/calculator if elif else.py
@@ -0,0 +1,36 @@
+while True:
+    print("Options:")
+    print("Enter 'add' for addition")
+    print("Enter 'sub' for subtraction")
+    print("Enter 'mul' for multiplication")
+    print("Enter 'div' for division")
+    print("Enter 'exit' to end the program")
+
+    user_input = input(": ")
+
+    if user_input == "exit":
+        break
+
+    if user_input not in ("add", "sub", "mul", "div"):
+        print("Invalid input. Please enter a valid option.")
+        continue
+
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+
+    result = None
+
+    if user_input == "add":
+        result = num1 + num2
+    elif user_input == "sub":
+        result = num1 - num2
+    elif user_input == "mul":
+        result = num1 * num2
+    elif user_input == "div" and num2 != 0:
+        result = num1 / num2
+    elif user_input == "div" and num2 == 0:
+        print("Error: Division by zero")
+        continue
+
+    if result is not None:
+        print("Result:", result)
