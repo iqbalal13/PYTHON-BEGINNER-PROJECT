698af12e37e63c48b0604fd4c0b9e52de1a2e90d
iqbalal13 <muhammadiqbalalmaududii@gmail.com>
2024-01-18 00:47:31 +0700

    Add files via upload

---
+++ PYRAMID/pyramid while double loop.py
@@ -0,0 +1,20 @@
+def print_pyramid():
+    while True:
+        height_input = input("Enter the height of the pyramid: ")
+
+        if height_input.isdigit():
+            height = int(height_input)
+            
+            i = 1
+            while i <= height:
+                spaces = " " * (height - i)
+                stars = "*" * (2 * i - 1)
+                print(spaces + stars)
+                i += 1
+            
+            break
+        else:
+            print("Please enter a valid integer for the height.")
+
+# Call the function to print the pyramid
+print_pyramid()
