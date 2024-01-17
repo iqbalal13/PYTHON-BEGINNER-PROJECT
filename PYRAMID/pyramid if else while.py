ed437027aa4ac3b76d857a41ac10f7330b5718f4
iqbalal13 <muhammadiqbalalmaududii@gmail.com>
2024-01-18 00:43:46 +0700

    Add files via upload

---
+++ PYRAMID/pyramid if else while.py
@@ -0,0 +1,17 @@
+def print_pyramid():
+    height_input = input("Enter the height of the pyramid: ")
+    
+    if height_input.isdigit():
+        height = int(height_input)
+        
+        i = 1
+        while i <= height:
+            spaces = " " * (height - i)
+            stars = "*" * (2 * i - 1)
+            print(spaces + stars)
+            i += 1
+    else:
+        print("Please enter a valid integer for the height.")
+
+# Call the function to print the pyramid
+print_pyramid()
