d924ffbe8d2632581ea98e880e41179b8083686b
iqbalal13 <muhammadiqbalalmaududii@gmail.com>
2024-01-21 02:43:08 +0700

    Add files via upload

---
+++ PRISM/prism while if eslse try excwpt quit.py
@@ -0,0 +1,30 @@
+def draw_prism(length, width, height):
+    # Draw the top face
+    print(" " * (width + 1) + "_" * length)
+
+    # Draw the sides
+    h = height
+    while h > 0:
+        try:
+            if h == height:
+                print(" " * h + "/" + " " * (length - 2) + "/")
+            elif h == 1:
+                raise StopIteration  # Terminate the loop when h equals 1
+            else:
+                print(" " * h + "/" + " " * (length - 2) + "/")
+            h -= 1
+        except StopIteration:
+            # Draw the bottom face
+            print("/" + "_" * (length - 2) + "/")
+
+# Get user input for prism dimensions
+length = int(input("Enter the length of the prism (enter -1 to quit): "))
+while length != -1:
+    width = int(input("Enter the width of the prism: "))
+    height = int(input("Enter the height of the prism: "))
+
+    # Draw the prism based on user input
+    draw_prism(length, width, height)
+
+    # Get user input for the next prism or to quit
+    length = int(input("Enter the length of the prism (enter -1 to quit): "))
