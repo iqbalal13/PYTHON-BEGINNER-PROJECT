c833a5b11551c14e27a1c2d669f82c9296059dcd
iqbalal13 <muhammadiqbalalmaududii@gmail.com>
2024-02-07 18:20:20 +0700

    Add files via upload

---
+++ CUBE PERIMETER/CUBE PERIMETER.py
@@ -0,0 +1,8 @@
+def cube_perimeter(side_length):
+    return 4 * side_length
+
+# Example usage:
+side_length = float(input("Enter the side length of the cube: "))
+perimeter = cube_perimeter(side_length)
+
+print(f"The perimeter of one face of the cube is: {perimeter}")
