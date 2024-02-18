297fc32e3f6638c3f2c9b7c4201956594dda91d4
iqbalal13 <muhammadiqbalalmaududii@gmail.com>
2024-02-18 21:42:19 +0700

    Add files via upload

---
+++ CUBE DRAW/cube 3d 2.py
@@ -0,0 +1,22 @@
+def draw_3d_cube(size):
+    i = 0
+    while i < size:
+        # Draw the top face
+        print(" " * (size - i - 1) + "*" * size)
+        i += 1
+
+    i = size - 2
+    while i >= 0:
+        # Draw the sides
+        print("*" + " " * (size - 2) + "*")
+        i -= 1
+
+    i = 0
+    while i < size:
+        # Draw the bottom face
+        print(" " * i + "*" * (size - i))
+        i += 1
+
+# Example usage:
+cube_size = int(input("Enter the size of the 3D cube: "))
+draw_3d_cube(cube_size)
