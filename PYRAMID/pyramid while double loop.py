b3bafee51e60c3a25206270a2f73f8095dd509ba
iqbalal13 <muhammadiqbalalmaududii@gmail.com>
2024-04-04 23:27:01 +0700

    Update pyramid while double loop.py

--- PYRAMID/pyramid while double loop.py
+++ PYRAMID/pyramid while double loop.py
@@ -4,15 +4,6 @@ def print_pyramid():
 
         if height_input.isdigit():
             height = int(height_input)
-            
-            i = 1
-            while i <= height:
-                spaces = " " * (height - i)
-                stars = "*" * (2 * i - 1)
-                print(spaces + stars)
-                i += 1
-            
-            break
         else:
             print("Please enter a valid integer for the height.")
 
