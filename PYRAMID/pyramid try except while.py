93e1aace0d1f911a6451332ba9bf8cb3f4a1a0a6
iqbalal13 <muhammadiqbalalmaududii@gmail.com>
2024-04-04 23:27:30 +0700

    Update pyramid try except while.py

--- PYRAMID/pyramid try except while.py
+++ PYRAMID/pyramid try except while.py
@@ -1,13 +1,7 @@
 def print_pyramid():
     try:
         height = int(input("Enter the height of the pyramid: "))
-        
-        i = 1
-        while i <= height:
-            spaces = " " * (height - i)
-            stars = "*" * (2 * i - 1)
-            print(spaces + stars)
-            i += 1
+    
     except ValueError:
         print("Please enter a valid integer for the height.")
 
