9d7c8dc72fa6120b2417a588628de07216ae09cf
iqbalal13 <muhammadiqbalalmaududii@gmail.com>
2024-04-04 23:24:17 +0700

    Update try except  inside if while.py

--- PYRAMID/try except  inside if while.py
+++ PYRAMID/try except  inside if while.py
@@ -4,18 +4,7 @@ def print_pyramid():
     if height_input.isdigit():
         height = int(height_input)
 
-        try:
-            if height > 0:
-                i = 1
-                while i <= height:
-                    spaces = " " * (height - i)
-                    stars = "*" * (2 * i - 1)
-                    print(spaces + stars)
-                    i += 1
-            else:
-                print("Please enter a positive integer for the height.")
-        except Exception as e:
-            print(f"An error occurred: {e}")
+       
     else:
         print("Please enter a valid integer for the height.")
 
