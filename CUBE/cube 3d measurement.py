6a815ad2f31bd490431614723ae6207c975d91ee
iqbalal13 <muhammadiqbalalmaududii@gmail.com>
2024-04-05 19:37:32 +0700

    Add files via upload

---
+++ CUBE/cube 3d measurement.py
@@ -0,0 +1,23 @@
+def draw_3d_cube(size, unit):
+    if size <= 0:
+        print("Invalid size. Please enter a positive integer.")
+        return
+
+    # Draw the top face
+    for i in range(size):
+        print(" " * (size - i - 1) + "*" * size)
+
+    # Draw the sides
+    for i in range(size - 2, -1, -1):
+        print("*" + " " * (size - 2) + "*")
+
+    # Draw the bottom face
+    for i in range(size):
+        print(" " * i + "*" * (size - i))
+
+    print(f"\nCube Size: {size} {unit}\n")
+
+# Example usage:
+cube_size = int(input("Enter the size of the 3D cube: "))
+unit = input("Enter the unit of measurement (e.g., cm, dm, mm): ")
+draw_3d_cube(cube_size, unit)
