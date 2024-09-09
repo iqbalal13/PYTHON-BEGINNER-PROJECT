f9ed2fb1e17997b500ebf410d0d68712aeb1f646
Iqbal Al Maududi <muhammadiqbalalmaududii@gmail.com>
2024-09-09 17:59:35 +0700

    Add files via upload

---
+++ OOP/OOP REVISION 4.py
@@ -0,0 +1,23 @@
+class Person:
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+    def introduce(self):
+        print("Hello, my name is", self.name, "and I am", self.age, "years old", self.gender)
+
+# Take user's input for name, age, and gender
+name = input("What is your name? ")
+gender = input("What is your gender? ")
+
+# Use try-except block to handle the case when user inputs a string instead of an integer for age
+try:
+    age = int(input("How old are you? "))
+except ValueError:
+    print("Please enter a valid integer for age.")
+    age = int(input("How old are you? "))
+
+# Create an object of the Person class using user's input
+person1 = Person(name, age, gender)
+person1.introduce()
