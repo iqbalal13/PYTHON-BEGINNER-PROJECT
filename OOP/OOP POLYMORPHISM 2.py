ecd558ae629c90a629f80e75c634cd4294cfaf21
Iqbal Al Maududi <muhammadiqbalalmaududii@gmail.com>
2024-09-09 17:57:18 +0700

    Add files via upload

---
+++ OOP/OOP POLYMORPHISM 2.py
@@ -0,0 +1,35 @@
+class Animal:
+    def __init__(self, name):
+        self.name = name
+
+    def make_sound(self):
+        pass
+
+
+class Dog(Animal):
+    def make_sound(self):
+        print(self.name + " says woof woof!!")
+
+
+class Cat(Animal):
+    def make_sound(self):
+        print(self.name + " says meow!!")
+
+
+while True:
+    animal_type = input("Enter the type of animal (dog/cat): ")
+    animal_name = input("Enter the name of the animal: ")
+    try:
+        int(animal_name)
+        print("Invalid animal name. Please enter a string.")
+    except ValueError:
+        break
+
+if animal_type.lower() == "dog":
+    animal = Dog(animal_name)
+elif animal_type.lower() == "cat":
+    animal = Cat(animal_name)
+else:
+    print("Invalid animal type")
+
+animal.make_sound()
