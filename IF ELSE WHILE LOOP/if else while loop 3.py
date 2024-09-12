1a2e87a136482795668b178f85fecd7712237d43
Iqbal Al Maududi <muhammadiqbalalmaududii@gmail.com>
2024-09-12 17:28:04 +0700

    Add files via upload

---
+++ IF ELSE WHILE LOOP/if else while loop 3.py
@@ -0,0 +1,15 @@
+mobil = ""
+while mobil != "exit":
+  mobil = str(input("masukkan perintah: "))
+  if mobil == "nyala":
+    print("mobil menyala")
+  elif mobil == "jalan":
+    print("mobil melaju")
+  elif mobil == "stop":
+    print("mobil berhenti")
+  elif mobil == "exit":
+    print("anda keluar dari program")
+    break
+  else:
+    print("Input Salah")
+    quit()
