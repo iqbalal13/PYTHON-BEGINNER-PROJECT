771dba02cbc071d7a19cf2147c0d63628310141f
Iqbal Al Maududi <muhammadiqbalalmaududii@gmail.com>
2024-09-12 17:28:21 +0700

    Add files via upload

---
+++ IF ELSE WHILE LOOP/if else while loop 4.py
@@ -0,0 +1,10 @@
+count = int(input("masukkan perintah: "))
+
+while True:
+    if count % 2 == 0:
+        print(count, "is even")
+    else:
+        print(count, "is odd")
+    count += 1
+    if count > 10:
+        break
