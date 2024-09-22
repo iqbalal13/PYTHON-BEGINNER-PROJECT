3519eec633fcb98fe4dc401208c3cd3ce27e420d
Iqbal Al Maududi <muhammadiqbalalmaududii@gmail.com>
2024-09-22 20:59:59 +0700

    Add files via upload

---
+++ TRY EXCEPT/try except 2.py
@@ -0,0 +1,8 @@
+try:
+    # code that may raise an exception
+    numerator = 10
+    denominator = 0
+    result = numerator / denominator
+except:
+    # code to handle the exception
+    print("An error occurred")
