b5c758eb8466c37eb46b0bd2e34af89c0cd50a1e
Iqbal Al Maududi <muhammadiqbalalmaududii@gmail.com>
2025-09-17 19:32:14 +0700

    Delete CUBE directory

--- CUBE/cube.py
+++
@@ -1,7 +0,0 @@
-def cube_volume(side_length):
-    return side_length ** 3
-
-# Example usage:
-side_length = float(input("Enter the side length of the cube: "))
-volume = cube_volume(side_length)
-print(f"The volume of the cube with side length {side_length} is: {volume}")
