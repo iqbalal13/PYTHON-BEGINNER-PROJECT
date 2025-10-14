d096d6fc13696cfb3c0dd497fa2ad321aca9a38d
Iqbal Al Maududi <muhammadiqbalalmaududii@gmail.com>
2025-10-14 17:49:08 +0700

    Delete PYRAMID directory

--- PYRAMID/pyramid try except while.py
+++
@@ -1,9 +0,0 @@
-def print_pyramid():
-    try:
-        height = int(input("Enter the height of the pyramid: "))
-    
-    except ValueError:
-        print("Please enter a valid integer for the height.")
-
-# Call the function to print the pyramid
-print_pyramid()
